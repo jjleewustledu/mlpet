428ccd209a245a6f9ecaffebfcb31578c9a65a20
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2020-01-14 18:06:27 -0600

    refactored SubjectResolve* to SubjectResolverTool

--- src/+mlpet/SubjectResolverToHO.m
+++ src/+mlpet/SubjectResolverToHO.m
@@ -1,36 +1,37 @@
-classdef SubjectResolverToFDG < handle & mlpet.SessionResolverToFDG
-	%% SUBJECTRESOLVERTOFDG  
+classdef SubjectResolverToHO < handle & mlpet.SessionResolverToHO
+	%% SUBJECTRESOLVERTOHO  
 
 	%  $Revision$
- 	%  was created 08-Jan-2020 22:25:16 by jjlee,
+ 	%  was created 14-Jan-2020 12:44:19 by jjlee,
  	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpet/src/+mlpet.
  	%% It was developed on Matlab 9.7.0.1261785 (R2019b) Update 3 for MACI64.  Copyright 2020 John Joowon Lee.
  	
-	methods         
+	methods
         function        constructResamplingRestricted(this, varargin)
             this.tools_.constructResamplingRestricted(varargin{:})
         end
         function fps  = linkAndSimplifyScans(this, varargin)
             fps = this.tools_.linkAndSimplifyScans(varargin{:});
-        end         
-		  
- 		function this = SubjectResolverToFDG(varargin)
- 			%% SUBJECTRESOLVERTOFDG
+        end 
+        
+ 		function this = SubjectResolverToHO(varargin)
+ 			%% SUBJECTRESOLVERTOHO
  			%  @param .
 
- 			this = this@mlpet.SessionResolverToFDG(varargin{:});
+ 			this = this@mlpet.SessionResolverToHO(varargin{:});
             this.collectionRB_ = mlfourdfp.CollectionResolveBuilder( ...
                 'sessionData', this.client_.sessionData, ...
                 'workpath', fullfile(this.client_.sessionData.subjectPath, '')); % replacing of superclass assignment
             this.tools_ = mlpet.SubjectResolverTools('resolver', this);
  		end
-    end 
+    end
     
-    %% PROTECTED
+    %% PROTECTED    
     
     properties (Access = protected)
         tools_
     end
 
 	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
-end
+ end
+
