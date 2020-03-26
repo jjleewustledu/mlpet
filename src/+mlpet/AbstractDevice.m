d84ca9dea18fe11159b4f5b815bc35241216d774
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2020-03-26 11:08:04 -0500

    Abstract classes are passing all unit tests

---
+++ src/+mlpet/AbstractDevice.m
@@ -0,0 +1,190 @@
+classdef (Abstract) AbstractDevice < handle & matlab.mixin.Copyable & mldata.ITiming & mlpet.ITracerData
+	%% DEVICE is the AbstractProduct in an abstract factory pattern.
+    %  See also factories mlpet.DeviceKit & descendent classes.
+    %  See also concrete products:  {mlpowers, mlarbelaez, mlraichle, ....}.{BloodSuckerDevice, CapracDevice, 
+    %  TwiliteDevice, BiographMMRDevice, EcatExactHRPlusDevice}.
+
+	%  $Revision$
+ 	%  was created 18-Oct-2018 13:58:30 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpet/src/+mlpet.
+ 	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+	properties (Dependent)
+        calibration
+        data
+        datetimeForDecayCorrection  
+        decayCorrected
+        logger
+        timeForDecayCorrection
+        
+        %% mldata.ITiming, mldata.TimingData
+        
+        datetime0
+        datetimeF
+        datetimeInterpolants
+        datetimeMeasured
+        datetimeWindow
+        datetimes
+        dt
+        index0
+        indexF
+        indices
+        taus
+        time0
+        timeF
+        timeInterpolants
+        times
+        timeWindow        
+    end    
+    
+    methods (Static)
+    end
+
+	methods 
+        
+        %% GET
+        
+        function g = get.calibration(this)
+            g = this.calibration_;
+        end
+        function g = get.data(this)
+            g = this.data_;
+        end
+        function g = get.datetimeForDecayCorrection(this)
+            g = this.data_.datetimeForDecayCorrection;
+        end
+        function g = get.decayCorrected(this)
+            g = this.data_.decayCorrected;
+        end
+        function g = get.logger(this)
+            g = this.logger_;
+        end
+        function g = get.timeForDecayCorrection(this)
+            g = this.data_.timeForDecayCorrection;
+        end        
+        
+        
+        function g = get.datetime0(this)
+            g = this.data_.datetime0;
+        end
+        function     set.datetime0(this, s)
+            this.data_.datetime0 = s;
+        end
+        function g = get.datetimeF(this)
+            g = this.data_.datetimeF;
+        end
+        function     set.datetimeF(this, s)
+            this.data_.datetimeF = s;
+        end
+        function g = get.datetimeInterpolants(this)
+            g = this.data_.datetimeInterpolants;
+        end
+        function g = get.datetimeMeasured(this)
+            g = this.data_.datetimeMeasured;
+        end
+        function     set.datetimeMeasured(this, s)
+            this.data_.datetimeMeasured = s;
+        end
+        function g = get.datetimes(this)
+            g = this.data_.datetimes;
+        end
+        function g = get.datetimeWindow(this)
+            g = this.data_.datetimeWindow;
+        end
+        function     set.datetimeWindow(this, s)
+            this.data_.datetimeWindow = s;
+        end
+        function g = get.dt(this)
+            g = this.data_.dt;
+        end
+        function     set.dt(this, s)
+            this.data_.dt = s;
+        end
+        function g = get.index0(this)
+            g = this.data_.index0;
+        end
+        function     set.index0(this, s)
+            this.data_.index0 = s;
+        end
+        function g = get.indexF(this)
+            g = this.data_.indexF;
+        end
+        function     set.indexF(this, s)
+            this.data_.indexF = s;
+        end
+        function g = get.indices(this)
+            g = this.data_.indices;
+        end
+        function g = get.taus(this)
+            g = this.data_.taus;
+        end  
+        function     set.taus(this, s)
+            this.data_.taus = s;
+        end
+        function g = get.time0(this)
+            g = this.data_.time0;
+        end
+        function     set.time0(this, s)
+            this.data_.time0 = s;
+        end
+        function g = get.timeF(this)
+            g = this.data_.timeF;
+        end
+        function     set.timeF(this, s)
+            this.data_.timeF = s;
+        end
+        function g = get.timeInterpolants(this)
+            %% GET.TIMEINTERPOLANTS are uniformly separated by this.dt
+            %  @returns interpolants this.times(1):this.dt:this.times(end)
+            
+            g = this.data_.timeInterpolants;
+        end
+        function g = get.times(this)
+            g = this.data_.times;
+        end
+        function     set.times(this, s)
+            this.data_.times = s;
+        end
+        function g = get.timeWindow(this)
+            g = this.data_.timeWindow;
+        end
+        function     set.timeWindow(this, s)
+            this.data_.timeWindow = s;
+        end  
+        
+        %%        
+        
+        function d = datetime(this, varargin)
+            d = this.data_.datetime(varargin{:});
+        end        
+        function d = duration(this, varargin)
+            d = this.data_.duration(varargin{:});
+        end
+        function     resetTimeLimits(this)
+            this.data_.resetTimeLimits();
+        end
+    end 
+    
+    %% PROTECTED
+    
+    properties (Access = protected)
+        calibration_
+        data_
+        logger_
+    end
+    
+    methods (Access = protected)
+        function this = AbstractDevice(varargin)
+            %% DEVICE
+            
+ 			ip = inputParser;
+            ip.KeepUnmatched = true;
+            parse(ip, varargin{:});
+            
+            this.logger_ = mlpipeline.Logger2(this);
+        end
+    end
+    
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
