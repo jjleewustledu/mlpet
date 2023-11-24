fc8918b7284611ec1bc1d82e71961dd7076c428e
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-11-24 14:21:20 -0600

    For simul. anneal., artery_interpolated → ArteryInterpolated, times_sampled → TimeSampled, measurement → Measurement

--- src/+mlpet/TracerSimulAnneal.m
+++ src/+mlpet/TracerSimulAnneal.m
@@ -7,7 +7,7 @@ classdef TracerSimulAnneal < mloptimization.SimulatedAnnealing
  	%% It was developed on Matlab 9.7.0.1434023 (R2019b) Update 6 for MACI64.  Copyright 2020 John Joowon Lee.
  	
 	properties
-        artery_interpolated
+        ArteryInterpolated
         ks0
         ks_lower
         ks_upper
@@ -45,36 +45,35 @@ classdef TracerSimulAnneal < mloptimization.SimulatedAnnealing
             conc_ = zeros(size(conc));
             conc_(1+Dt:end) = conc(1:end-Dt);
             conc = conc_;
+
+    methods %% GET
+        function g = get.ks(this)
+            assert(~isempty(this.product_))
+            g = this.product_.ks;
         end
     end
 
 	methods	
-        
-        %% GET
-        
-        function g = get.ks(this)
-            g = this.results_.ks;
-        end
-        
-        %%
-        
  		function this = TracerSimulAnneal(varargin)
  			this = this@mloptimization.SimulatedAnnealing(varargin{:});
+            
+            [this.ks_lower,this.ks_upper,this.ks0] = remapper(this);
+            this.ArteryInterpolated = this.model.artery_interpolated;
         end
         
         function disp(this)
             fprintf('\n')
             fprintf(class(this))
-            if isempty(this.results_)
+            if isempty(this.product_)
                 return
             end
-            fprintf('initial ks0: '); disp(this.results_.ks0)
-            fprintf('est.     ks: '); disp(this.results_.ks)
-            fprintf('        sse: '); disp(this.results_.sse)
-            fprintf('   exitflag: '); disp(this.results_.exitflag)
-            disp(this.results_.output)
-            disp(this.results_.output.rngstate)
-            disp(this.results_.output.temperature)
+            fprintf('initial ks0: '); disp(this.product_.ks0)
+            fprintf('est.     ks: '); disp(this.product_.ks)
+            fprintf('        sse: '); disp(this.product_.sse)
+            fprintf('   exitflag: '); disp(this.product_.exitflag)
+            disp(this.product_.output)
+            disp(this.product_.output.rngstate)
+            disp(this.product_.output.temperature)
         end
         function aif1 = dispersedAif(this, aif)
             n = length(aif);
@@ -100,12 +99,12 @@ classdef TracerSimulAnneal < mloptimization.SimulatedAnnealing
             end
         end
         function Q = loss(this)
-            Q = this.results_.sse;
+            Q = this.product_.sse;
         end
         function h = plot(this, varargin)
             ip = inputParser;
             addParameter(ip, 'showAif', true, @islogical)
-            addParameter(ip, 'xlim', [-30 1000], @isnumeric)            
+            addParameter(ip, 'xlim', [-10 500], @isnumeric)            
             addParameter(ip, 'ylim', [], @isnumeric)
             addParameter(ip, 'zoom', 4, @isnumeric)
             parse(ip, varargin{:})
@@ -113,16 +112,16 @@ classdef TracerSimulAnneal < mloptimization.SimulatedAnnealing
             this.zoom = ipr.zoom;            
             
             ad = mlaif.AifData.instance();
-            tBuffer = ad.tBuffer;           
-            aif = this.dispersedAif(this.artery_interpolated);
+            tBuffer = ad.tBuffer;
+            aif = this.dispersedAif(this.ArteryInterpolated, this.Delta);
             h = figure;
-            times = this.times_sampled;
-            sampled = this.model.sampled(this.ks, this.v1, aif, times);
+            times = this.TimesSampled;
+            sampled = this.model.sampled(this.ks, this.Data, aif, times);            
             
             if ipr.zoom > 1
-                leg_meas = sprintf('measurement x%i', ipr.zoom);
+                leg_meas = sprintf('Measurement x%i', ipr.zoom);
             else
-                leg_meas = 'measurement';
+                leg_meas = 'Measurement';
             end
             if ipr.zoom > 1
                 leg_est = sprintf('estimation x%i', ipr.zoom);
@@ -132,7 +131,7 @@ classdef TracerSimulAnneal < mloptimization.SimulatedAnnealing
             if ipr.showAif
                 plot(times, ipr.zoom*this.Measurement, ':o', ...
                     times(1:length(sampled)), ipr.zoom*sampled, '-', ...
-                    -tBuffer:length(aif)-tBuffer-1, aif, '--')   
+                    -tBuffer:length(aif)-tBuffer-1, aif, '--') 
                 legend(leg_meas, leg_est, 'aif')
             else
                 plot(times, ipr.zoom*this.Measurement, 'o', ...
@@ -143,16 +142,57 @@ classdef TracerSimulAnneal < mloptimization.SimulatedAnnealing
             if ~isempty(ipr.ylim); ylim(ipr.ylim); end
             xlabel('times / s')
             ylabel('activity / (Bq/mL)')
-            annotation('textbox', [.175 .5 .3 .3], 'String', sprintfModel(this), 'FitBoxToText', 'on', 'FontSize', 8, 'LineStyle', 'none')
+            annotation('textbox', [.25 .5 .3 .3], 'String', sprintfModel(this), 'FitBoxToText', 'on', 'FontSize', 8, 'LineStyle', 'none')
             dbs = dbstack;
             title(dbs(1).name)
-        end
+        end 
         function save(this)
             save([this.fileprefix '.mat'], this);
         end
         function saveas(this, fn)
             save(fn, this);
-        end      
+        end
+        function this = solve(this, varargin)
+            ip = inputParser;
+            addRequired(ip, 'loss_function', @(x) isa(x, 'function_handle'))
+            parse(ip, varargin{:})
+            ipr = ip.Results;
+            
+            options_fmincon = optimoptions('fmincon', ...
+                'FunctionTolerance', 1e-9, ...
+                'OptimalityTolerance', 1e-9);
+            if this.visualize_anneal
+                options = optimoptions('simulannealbnd', ...
+                    'AnnealingFcn', 'annealingboltz', ...
+                    'FunctionTolerance', eps, ...
+                    'HybridFcn', {@fmincon, options_fmincon}, ...
+                    'InitialTemperature', 20, ...
+                    'ReannealInterval', 200, ...
+                    'TemperatureFcn', 'temperatureexp', ...
+                    'Display', 'diagnose', ...
+                    'PlotFcns', {@saplotbestx,@saplotbestf,@saplotx,@saplotf,@saplotstopping,@saplottemperature});
+            else
+                options = optimoptions('simulannealbnd', ...
+                    'AnnealingFcn', 'annealingboltz', ...
+                    'FunctionTolerance', eps, ...
+                    'HybridFcn', {@fmincon, options_fmincon}, ...
+                    'InitialTemperature', 20, ...
+                    'ReannealInterval', 200, ...
+                    'TemperatureFcn', 'temperatureexp');
+            end
+ 			[ks_,sse,exitflag,output] = simulannealbnd( ...
+                @(ks__) ipr.loss_function( ...
+                        ks__, this.Data, this.ArteryInterpolated, this.TimesSampled, double(this.Measurement)), ...
+                        this.ks0, this.ks_lower, this.ks_upper, options);
+            
+            this.product_ = struct('ks0', this.ks0, 'ks', ks_, 'sse', sse, 'exitflag', exitflag, 'output', output); 
+            if ~this.quiet
+                fprintfModel(this)
+            end
+            if this.visualize
+                plot(this)
+            end
+        end
         function s = sprintfModel(this)
             s = sprintf('Simulated Annealing:\n');
             for ky = 1:length(this.ks)
